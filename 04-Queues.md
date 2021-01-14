318cee8835c6f9121646a70a60dee36a018986fc
Anuj Rajput <anujrajput04@gmail.com>
2021-01-14 17:14:06 +0530

    Ring buffer implementation of queue

--- 04-Queues.md
+++ 04-Queues.md
@@ -60,3 +60,11 @@ The ring buffer has two pointers that keep track of two things:
 
 Each time that you add an item to the queue, the write pointer increments by one.
 Whenever the read and write pointers are at the same index, that means the queue is empty.
+
+Operations|Best case|Worst case
+-------------|-----------|-------------
+enqueue(_:)|O(1)|O(1)
+dequeue(_:)|O(1)|O(1)
+Space Complexity|O(n)|O(n)
+
+The ring buffer based queue has the same time complexity for enqueue and dequeue as the linked list implementation. The only difference is the space complexity. The ring buffer has a fixed size, which means that enqueue can fail.
--- Projects/08-Queues/Queue.playground/Pages/QueueRingBuffer.xcplaygroundpage/Contents.swift
+++ Projects/08-Queues/Queue.playground/Pages/QueueRingBuffer.xcplaygroundpage/Contents.swift
@@ -1,1 +1,39 @@
+public struct QueueRingBuffer<T>: Queue {
+    
+    private var ringBuffer: RingBuffer<T>
+    
+    public init(count: Int) {
+        ringBuffer = RingBuffer<T>(count: count)
+    }
+    
+    public var isEmpty: Bool {
+        ringBuffer.isEmpty
+    }
+    
+    public var peek: T? {
+        ringBuffer.first
+    }
+    
+    public mutating func enqueue(_ element: T) -> Bool {
+        ringBuffer.write(element)
+    }
+    
+    public mutating func dequeue() -> T? {
+        isEmpty ? nil : ringBuffer.read()
+    }
+}
 
+extension QueueRingBuffer: CustomStringConvertible {
+    public var description: String {
+        String(describing: ringBuffer)
+    }
+}
+
+var queue = QueueRingBuffer<String>(count: 10)
+queue.enqueue("Aj")
+queue.enqueue("Any")
+queue.enqueue("Chunk")
+queue
+queue.dequeue()
+queue
+queue.peek

aa0e1d7fbb25d932593628f32e093b8196d8efd7
Anuj Rajput <anujrajput04@gmail.com>
2021-02-18 12:01:56 +0530

    Added Trees

---
+++ Projects/10-Trees/GeneralPurposeTree.playground/Sources/TreeNode.swift
@@ -0,0 +1,13 @@
+public class TreeNode<T> {
+    public var value: T
+    public var children: [TreeNode] = []
+    
+    public init(_ value: T) {
+        self.value = value
+    }
+    
+    // Add a child node to a node
+    public func add(_ child: TreeNode) {
+        children.append(child)
+    }
+}
